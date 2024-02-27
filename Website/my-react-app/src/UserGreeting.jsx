da1144d71f039fff822b1c293d94c4751fdbbcd3
sidshra <amsapriya@yahoo.com>
2024-02-27 23:00:47 +1300

    UserGreeting - Conditional render-3

--- Website/my-react-app/src/UserGreeting.jsx
+++ Website/my-react-app/src/UserGreeting.jsx
@@ -3,8 +3,8 @@
     (show/hide/change components)  */
 
 function UserGreeting(props){
-    return (props.isLoggedIn ? <h2>Welcome {props.username}</h2>
-                             : <h2>Please log in to continue</h2>
+    return (props.isLoggedIn ? <h2 className="welcome-message">Welcome {props.username}</h2>
+                             : <h2 className="login-prompt">Please log in to continue</h2>
     );
     
 }
