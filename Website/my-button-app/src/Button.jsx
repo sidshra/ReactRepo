3d48a69d076896f1e63d3766d85419bf58f11a7b
sidshra <amsapriya@yahoo.com>
2024-02-27 13:20:58 +1300

    Button-Inline CSS

---
+++ Website/my-button-app/src/Button.jsx
@@ -0,0 +1,19 @@
+function Button(){
+
+    const styles = {
+    backgroundColor: "hsl(193, 69%, 24%)",
+    color: "white",
+    padding: "10px 20px",
+    borderRadius: "5px",
+    border: "none",
+    cursor: "pointer",
+    }
+
+    return(
+
+       <button style={styles}>Click Me</button>
+       
+    );
+}
+
+export default Button
