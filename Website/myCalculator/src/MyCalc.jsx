8e5bea53fee389925087522f9f9d183f6becb3e6
sidshra <amsapriya@yahoo.com>
2024-03-04 21:13:19 +1300

    My calculator - 2nd commit

--- Website/myCalculator/src/MyCalc.jsx
+++ Website/myCalculator/src/MyCalc.jsx
@@ -1,25 +1,36 @@
+
+
 function myCalc(){
 
 
-const displayVal= 0;
+    const displayVal= 0;
 
-return(
-    <div><h2>My Calculator</h2>
-    <main>
-        <span><h3 id="display">0</h3></span>
-        <span><button id="clear">AC</button><button id="divide">/</button><button id="multiply">X</button></span>
+    const reset = () => {
+        displayVal= 0;
+    }
+    
+    return(
+    <div>    <main>
+        <span><h3 id="display">{displayVal}</h3></span>
+        <span><button id="clear" onClick={reset}>AC</button><button id="divide" >/</button><button id="multiply">X</button></span>
         
-        <span><button id="seven">7</button><button id="eight">8</button><button id="nine">9</button><button id="minus">-</button></span>
+        <span><button id="seven" value="7">7</button><button id="eight">8</button><button id="nine">9</button><button id="minus">-</button></span>
             <span><button id="four">4</button><button id="five">5</button><button id="six">6</button><button id="plus">+</button></span>
-            <span><span><div id="secdiv"><span><span><button id="one">1</button><button id="two">2</button><button id="three">3</button></span>
+            
+            <div className="secdiv"><div><span><button id="one">1</button><button id="two">2</button><button id="three">3</button></span>
                 
                 <span><button id="zero">0</button><button id="decimal">.</button></span>
-            </span></div></span>
-            <span><button id="equals">=</button></span></span>
+            </div>
+                <span id="a"><button id="equals">=</button></span></div>
+            
+        </main>    
+        <footer>Designed & Coded By<br />
+                        Priya
+        </footer>
            
 
     
-    </main>
+    
     </div>
 
 );
